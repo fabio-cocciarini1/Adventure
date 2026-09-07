1ed254589e55710bd2ad53522ae7d71c952f6d68
manjaro <fabio.cocciarini@studenti.unicam.it>
2026-09-07 10:38:25 +0200

    finestra di prova con pulsane per salvare e statistiche

--- app/src/main/java/it/unicam/cs/mpgc/rpg126109/Main.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126109/Main.java
@@ -1,24 +1,13 @@
 package it.unicam.cs.mpgc.rpg126109;
 
+import it.unicam.cs.mpgc.rpg126109.gui.Finestra;
+
 import java.awt.*;
 import javax.swing.JFrame;
 
 public class Main{
 	public static void main(String[] args){
-		//inizializzazione del jframe
-		JFrame finestra = new JFrame();
-		finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		finestra.setResizable(false);
-		finestra.setTitle("Adventure");
-		
-		//inizializzazione contenuto finestra
-		Finestra f = new Finestra();
-
-		finestra.add(f);
-		//finestra.setLocationRelativeTo();
-
-		finestra.pack();
-		finestra.setVisible(true);
+		Finestra finestraProva = new Finestra(800,200);
 	}
 
 }
--- app/src/main/java/it/unicam/cs/mpgc/rpg126109/gui/Finestra.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126109/gui/Finestra.java
@@ -1,19 +1,36 @@
-package it.unicam.cs.mpgc.rpg126109;
+package it.unicam.cs.mpgc.rpg126109.gui;
 
 import java.awt.*;
 import javax.swing.*;
 
-public class Finestra extends JPanel{
-	public int blocco = 16;
-	public int scala = 3;
-	public int larghezzaInBlocchi = 16;
-	public int altezzaInBlocchi = 12;
-	public int larghezzaSchermo = larghezzaInBlocchi * (blocco * scala);
-	public int altezzaSchermo = altezzaInBlocchi * (blocco * scala);
+public class Finestra extends JFrame{
 
-	public Finestra(){
-		this.setPreferredSize(new Dimension(larghezzaSchermo, altezzaSchermo));
-		this.setBackground(Color.BLACK);
-		this.setFocusable(true);
+	Container contenutoPane = null;
+
+	public Finestra(int larghezza,int altezza){
+		JFrame finestra = new JFrame("Finestra");
+		finestra.setBounds(500, 500, larghezza, altezza);
+		contenutoPane = finestra.getContentPane();
+		
+		//componenti
+		JLabel vita = new JLabel("vita : 100%");
+		JLabel difesa = new JLabel("difesa = 0");
+		JLabel attacco = new JLabel("attacco =  1");
+
+		JButton salva = new JButton("Salva");
+		
+		JPanel barraSuperiore = new JPanel(new FlowLayout());
+
+		barraSuperiore.add(salva);
+		barraSuperiore.add(vita);
+		barraSuperiore.add(difesa);
+		barraSuperiore.add(attacco);
+
+		
+
+		finestra.add(barraSuperiore, BorderLayout.NORTH);
+
+		finestra.setVisible(true);
+		finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
